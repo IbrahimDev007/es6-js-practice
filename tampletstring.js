b1fee5a65915923d53aa40c12a8b334fec7d3f3f
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-21 13:21:44 +0600

    aray reference distructing

--- tampletstring.js
+++ tampletstring.js
@@ -4,3 +4,14 @@ const str3=`patric`;
 console.log(str1,str2,str3);
 //  arrow function
 let add = (first,seceond) => first+seceond;
+//array reference problem
+
+let number=[56,4,55];
+let numbers=number;
+numbers.push(4,5)
+console.log(number,numbers);//same value
+numbers=[...number,45];
+console.log(numbers);
+// distructuring
+const  {x,y,...c}={x:2,y:3,name:'sakib' ,saleery:22};
+console.log(c);
