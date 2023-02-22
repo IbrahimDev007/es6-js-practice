23aa2da428d0d03fc53454bdbaac797194488d48
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-22 16:57:25 +0600

    map find filter and problem solving

---
+++ problemSolving.js
@@ -0,0 +1,59 @@
+// // problem 2 str length even odd
+// let even =(arr)=>{
+//     let newArr=[]
+//    for (const name of arr) {
+   
+//      if(name.length%2 == 0)
+//     newArr.push(name)
+//    } 
+//    return newArr;
+// }
+// console.log(even(['salam','kalam','jamila','kawser']));
+
+// //problem 3 sqr eVERY ELEMENT  calculate squre element  avarege square element;
+
+// let sqrAvg=(sqr)=>{
+//     let sqrs=sqr[0];
+    
+// for (let i = 0; i < sqr.length; i++) {
+//    if(i)
+//     sqrs=sqrs+sqr[i+1]
+//     console.log(sqrs);
+// };
+
+
+// }
+
+// console.log(sqrAvg([2,3,6,23,55]));
+
+
+// problem 4 
+//map
+const arrOdd =[ 1, 3, 5, 7, 9 ];
+
+const addOdd=arrOdd.map((arr)=>arr+1);
+
+console.log(addOdd);
+
+// for loop section 
+
+let arrOutput=[]
+for (let index = 0; index < arrOdd.length; index++) {
+    const element = array[index]+1;
+     arrOutput.push(element)    
+}
+console.log(arrOutput);
+//2nd section 
+//filter
+const arrNum= [33, 50, 79, 78, 90, 101, 30 ];
+const arr10=arrNum.filter(arr=>arr%10);
+console.log(arr10);
+// 3rd section 
+// find 
+const arrFin=arrNum.find(arr=>arr%10);
+console.log(arrFin);
+
+// problem 5 reduce 
+
+//add with reduce
+
