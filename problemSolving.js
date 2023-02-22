c54748ad8c6c4091dfb045bc4ca09eb0c8a03cb8
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-22 19:54:11 +0600

    reduce and some problem solving

--- problemSolving.js
+++ problemSolving.js
@@ -1,30 +1,29 @@
-// // problem 2 str length even odd
-// let even =(arr)=>{
-//     let newArr=[]
-//    for (const name of arr) {
+// problem 2 str length even odd
+let even =(arr)=>{
+    let newArr=[]
+   for (const name of arr) {
    
-//      if(name.length%2 == 0)
-//     newArr.push(name)
-//    } 
-//    return newArr;
-// }
-// console.log(even(['salam','kalam','jamila','kawser']));
+     if(name.length%2 == 0)
+    newArr.push(name)
+   } 
+   return newArr;
+}
+console.log(even(['salam','kalam','jamila','kawser']));
 
-// //problem 3 sqr eVERY ELEMENT  calculate squre element  avarege square element;
+//problem 3 sqr eVERY ELEMENT  calculate squre element  avarege square element;
 
-// let sqrAvg=(sqr)=>{
-//     let sqrs=sqr[0];
+let sqrAvg = (sqr)=>{
+    let sqrs=0;
     
-// for (let i = 0; i < sqr.length; i++) {
-//    if(i)
-//     sqrs=sqrs+sqr[i+1]
-//     console.log(sqrs);
-// };
+for (let i = 0; i < sqr.length; i++) {
+    sqrs=sqrs+Math.pow(sqr[i],2)
 
+};
+return sqrs/sqr.length
 
-// }
+}
 
-// console.log(sqrAvg([2,3,6,23,55]));
+console.log(sqrAvg([2,3,6,23,55]));
 
 
 // problem 4 
@@ -39,7 +38,7 @@ console.log(addOdd);
 
 let arrOutput=[]
 for (let index = 0; index < arrOdd.length; index++) {
-    const element = array[index]+1;
+    const element = arrOdd[index]+1;
      arrOutput.push(element)    
 }
 console.log(arrOutput);
@@ -56,4 +55,26 @@ console.log(arrFin);
 // problem 5 reduce 
 
 //add with reduce
+reduceNum=[ 1, 9, 17, 22 ]
+let sum=0;
+let reduceAdd=[ 1, 9, 17, 22 ].reduce((cur,prev) => (cur+prev),0)
+console.log(reduceAdd);
+for (let index = 0; index < reduceNum.length; index++) {
+    const prev=reduceNum[index];
+    sum=sum+prev;
+    console.log(sum);
+}
+console.log(sum);
+
+//reduce in obj properties
+const pepole=[
+    {name:'mena',age:20},
+    {name:'Rina',age:15},
+    {name:'Suchorita',age:22},
+]
+
+let ageAdd=pepole.reduce((curr,prev)=>(curr+prev.age),0);
+console.log(ageAdd);
+
+
 
